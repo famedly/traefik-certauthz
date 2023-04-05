6598e3542154ef9ca84dd64b82529d33f38357e7
Lars Kaiser <lars@kaiser.yt>
2023-04-05 17:20:17 +0200

    feat: add domain list to config

--- certauthz.go
+++ certauthz.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"regexp"
 	"net/http"
+	"strings"
 )
 
 type Config struct {
-	Regex string `json:"sanDnsPermitRegex,omitempty"`
+	Regex string
+	Domains []string
 }
 
 func CreateConfig() *Config {
 	return &Config{
 		Regex: "",
+		Domains: nil,
 	}
 }
 
@@ -24,7 +27,32 @@ type CertAuthz struct {
 }
 
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	var compiled, err = regexp.Compile(config.Regex)
+	if config.Regex != "" && len(config.Domains) != 0 {
+		return nil, fmt.Errorf("You must specify either a regex or a domain list, not both")
+	}
+	if config.Regex == "" && len(config.Domains) == 0 {
+		return nil, fmt.Errorf("You must specify either a regex or a domain list")
+	}
+
+	var to_compile = config.Regex
+	if len(config.Domains) > 0 {
+		domains_regexes := []string{}
+		for _, domain := range config.Domains {
+			// Rudimentary check for invalid chars in domain
+			// Invalid domain names are still possible
+			var invalid = regexp.MustCompile(`(?i)[^a-z0-9\-\.\*]`)
+			if invalid.MatchString(domain) {
+				return nil, fmt.Errorf("Invalid characters in domain name: %v", domain)
+			}
+			domain = strings.Replace(domain, ".", "[.]", -1)
+			domain = strings.Replace(domain, "*", "[^.]+", -1)
+			domain = "(?i)^" + domain + "$"
+			domains_regexes = append(domains_regexes, domain)
+		}
+		to_compile = strings.Join(domains_regexes, "|")
+	}
+
+	var compiled, err = regexp.Compile(to_compile)
 	if err != nil {
 		return nil, err
 	}
